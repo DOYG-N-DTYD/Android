4187be9850d48c9703c538864b452af7d670a3de
DOYG-N-DTYD <miroslawzborowski1105@gmail.com>
2023-10-12 20:34:21 +0200

    tests ok

--- app/src/main/java/com/example/drewmetapp/apis/ImgurAPIv3.kt
+++ app/src/main/java/com/example/drewmetapp/apis/ImgurAPIv3.kt
@@ -1,4 +1,15 @@
 package com.example.drewmetapp.apis
 
-class ImgurAPIv3 {
+import com.example.drewmetapp.models.GalleryResponse
+import com.example.drewmetapp.models.GalleryTagsResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ImgurAPIv3 {
+
+    @GET("gallery/hot?album_peviews=true")
+    fun getGalery() : Call<GalleryResponse>
+
+    @GET("tags")
+    fun getTags() : Call<GalleryTagsResponse>
 }
